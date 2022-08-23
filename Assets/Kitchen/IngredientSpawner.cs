c503626735e2a99b849c921b36bca412b9845cfe
Thomas Bombrun <thomas.bombrun@gmail.com>
2022-08-23 22:33:13 +0200

    setup ingredient spawner

---
+++ Assets/Kitchen/IngredientSpawner.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IngredientSpawner : MonoBehaviour
+{
+	public GameObject ingredientPrefab;
+
+
+	void Start()
+	{
+		InvokeRepeating("SpawnIngredient", 0f, 5f);
+	}
+
+
+
+	// Update is called once per frame
+	void SpawnIngredient()
+	{
+		if(transform.childCount == 0)
+		{
+			GameObject ingredient = Instantiate(ingredientPrefab, transform);
+			ingredient.name = ingredientPrefab.name;
+		}
+	}
+}
