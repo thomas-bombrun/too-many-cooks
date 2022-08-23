56ff74d1d424d46dc8491c170d5d2a5929d277fd
Thomas Bombrun <thomas.bombrun@gmail.com>
2022-08-23 02:45:27 +0200

    a cook can grab ingredients and move them around

---
+++ Assets/Cooks/PlayerControl.cs
@@ -0,0 +1,79 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Animator))]
+[RequireComponent(typeof(CharacterController))]
+public class PlayerControl : MonoBehaviour
+{
+    public float inputThreshold;
+    public float speed;
+    public GameObject rightHand;
+    public float grabRadius;
+
+    private Animator animator;
+    private CharacterController characterController;
+    private GameObject grabbedIngredient;
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+        characterController = GetComponent<CharacterController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        HandleControl();
+        HandleControlGrab();
+    }
+
+    void HandleControl()
+    {
+        float verticalInput = Input.GetAxis("Vertical");
+        float horizontalInput = Input.GetAxis("Horizontal");
+        if (Mathf.Abs(verticalInput) > inputThreshold || Mathf.Abs(horizontalInput) > inputThreshold)
+        {
+            animator.SetBool("isRunning", true);
+            Vector3 moveVector = new Vector3(verticalInput, 0, -horizontalInput) * speed;
+            characterController.Move(moveVector);
+            transform.LookAt(this.transform.position + moveVector);
+        }
+        else
+        {
+            animator.SetBool("isRunning", false);
+        }
+    }
+
+    void HandleControlGrab()
+    {
+        if (!Input.GetButtonDown("Fire1"))
+        {
+            return;
+        }
+        Debug.Log(grabbedIngredient);
+        if (grabbedIngredient == null)
+        {
+            Collider[] ingredients = Physics.OverlapSphere(transform.position, grabRadius, 1 << 6); // 1<<6 means layer number 6, layer number 6 is Ingredients. Great API, Unity !
+            foreach (Collider ingredient in ingredients)
+            {
+                grabbedIngredient = ingredient.gameObject;
+                grabbedIngredient.transform.parent = rightHand.transform;
+                grabbedIngredient.transform.localPosition = Vector3.zero;
+                grabbedIngredient.transform.rotation = Quaternion.identity;
+                return;
+            }
+        }
+        else
+        {
+            Collider[] plates = Physics.OverlapSphere(transform.position, grabRadius, 1 << 7); // 1<<7 means layer number 7, layer number 7 is Plates. Great API, Unity !
+            foreach (Collider plate in plates)
+            {
+                grabbedIngredient.transform.parent = plate.transform;
+                grabbedIngredient.transform.localPosition = Vector3.zero;
+                grabbedIngredient.transform.rotation = Quaternion.identity;
+                grabbedIngredient = null;
+                return;
+            }
+        }
+    }
+}
