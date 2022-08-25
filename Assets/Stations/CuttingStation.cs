f5b0ed9ca903480402d6b7d24742f9a5b8292586
Thomas Bombrun <thomas.bombrun@gmail.com>
2022-08-25 18:18:02 +0200

    add cheese wheel, cutting stations and cheeseburger

---
+++ Assets/Stations/CuttingStation.cs
@@ -0,0 +1,59 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+[System.Serializable]
+public struct CuttingTransformation
+{
+	public GameObject fromPrefab;
+	public GameObject toPrefab;
+}
+
+public class CuttingStation : Station
+{
+
+	public Transform IngredientSpot;
+	public List<CuttingTransformation> possibleTransformations;
+	private CuttingTransformation currentTransformation;
+
+	protected override bool WorkCanBeStarted(CookControl cook)
+	{
+		if(IngredientSpot.transform.childCount == 0)
+		{
+			return false;
+		}
+		GameObject ingredient = IngredientSpot.GetChild(0).gameObject;
+		foreach(var transformation in possibleTransformations)
+		{
+			if(ingredient.name == transformation.fromPrefab.name)
+			{
+				currentTransformation = transformation;
+				return true;
+			}
+		}
+		return false;
+	}
+
+	protected override void WorkDone(CookControl cook)
+	{
+		if(IngredientSpot.transform.childCount == 0)
+		{
+			//TODO handle better ?
+			Debug.LogWarning("Cutting board has lost its ingredient during work time");
+			return;
+		}
+		GameObject ingredient = IngredientSpot.GetChild(0).gameObject;
+		if(ingredient.name != currentTransformation.fromPrefab.name)
+		{
+			//TODO handle better ?
+			Debug.LogWarning("Cutting board has lost its ingredient during work time");
+			return;
+		}
+		Destroy(ingredient);
+		GameObject newIngredient = Instantiate(currentTransformation.toPrefab, IngredientSpot);
+		newIngredient.name = currentTransformation.toPrefab.name;
+		currentTransformation = new CuttingTransformation { };
+	}
+
+}
