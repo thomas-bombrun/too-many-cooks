77268db2dcbc232c8bac8248fd583f597270fa3f
uurriola <uurriola@wiremind.io>
2022-08-28 10:16:50 +0200

    feat: load last level on lost

--- Assets/Scenes/LevelManager.cs
+++ Assets/Scenes/LevelManager.cs
@@ -3,13 +3,24 @@ using UnityEngine.SceneManagement;
 
 public class LevelManager : MonoBehaviour
 {
+	public static int levelIndex = 1;
+
 	public static void LoadFirstLevel()
 	{
+		levelIndex = 1;
 		SceneManager.LoadScene("Intro - Level 0");
 	}
+
+
+	public static void LoadPreviousLevel()
+	{
+		SceneManager.LoadScene(levelIndex - 1);
+	}
+
 	public static void LoadNextLevel()
 	{
-		SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+		levelIndex = SceneManager.GetActiveScene().buildIndex + 1;
+		SceneManager.LoadScene(levelIndex);
 	}
 
 	public static void LoadGameOver()
